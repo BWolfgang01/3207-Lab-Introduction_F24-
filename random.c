261ab96912753c234a942a5ee0191b65aff79a2a
BWolfgang01 <tun39109@temple.edu>
2025-01-15 14:28:58 -0500

    Fixed random.c and added random.c to print_random.c

--- random.c
+++ random.c
@@ -1,16 +1,6 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
-
-char randchar();
-
-int main() {
-    char randomCharacter = randchar();
-    printf("%c", randomCharacter);
-}
 
 char randchar(){
-    srand((unsigned)time(NULL));
     char letters[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     //generates a number 0-26 and accesses the value at that index in letters
     char randletter = letters[rand() %26];
